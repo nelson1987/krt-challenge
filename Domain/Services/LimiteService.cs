9686303d1ace594a25cd687d47f615a0e238bc93
Nelson Neto <nelson.ash@outlook.com>
2024-10-28 16:15:07 -0300

    Criacao LimiteService

---
+++ Domain/Services/LimiteService.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Entities;
+using Domain.Helpers;
+using Domain.Repositories;
+
+namespace UnitTests;
+
+public class LimiteService
+{
+    private readonly ILimiteRepository _limiteRepository;
+
+    public LimiteService(ILimiteRepository limiteRepository)
+    {
+        _limiteRepository = limiteRepository;
+    }
+
+    public async Task Create(string documento, string agencia, string conta, decimal valor)
+    {
+        //Verifica se existe, se existir retornar um erro
+        var limiteExistente = await _limiteRepository.Buscar(documento, agencia, conta);
+        if (limiteExistente != null)
+            throw new Exception(Mensagem.LIMITE_CADASTRADO);
+        //Se não existir, criar
+        var limite = new Limite(documento, agencia, conta, valor);
+        await _limiteRepository.Incluir(limite);
+    }
+}
---
+++ UnitTests/LimiteServiceUnitTests.cs
@@ -0,0 +1,82 @@
+﻿using AutoFixture;
+using AutoFixture.AutoMoq;
+using Domain.Entities;
+using Domain.Repositories;
+using Moq;
+
+namespace UnitTests;
+
+public class LimiteServiceUnitTests
+{
+    private readonly IFixture _fixture = new Fixture()
+        .Customize(new AutoMoqCustomization
+        {
+            ConfigureMembers = true
+        });
+
+    private readonly LimiteService _sut;
+
+    public LimiteServiceUnitTests()
+    {
+        //var limite = _fixture.Create<Limite>();
+        _fixture.Freeze<Mock<ILimiteRepository>>()
+        .Setup(client => client.Buscar(
+            It.IsAny<string>(),
+            It.IsAny<string>(),
+            It.IsAny<string>()))
+        .ReturnsAsync((Limite?)null);
+
+        _sut = _fixture.Create<LimiteService>();
+    }
+
+    [Fact]
+    public async Task IncluirLimite_DadosValidos_RetornaDadosInseridos()
+    {
+        var documento = "Documento";
+        var agencia = "Agencia";
+        var conta = "Conta";
+        var valor = 0.00M;
+
+        await _sut.Create(documento, agencia, conta, valor);
+
+        _fixture.Freeze<Mock<ILimiteRepository>>()
+                .Verify(x =>
+                x.Buscar(
+                    It.IsAny<string>(),
+                    It.IsAny<string>(),
+                    It.IsAny<string>())
+                    , Times.Once);
+        _fixture.Freeze<Mock<ILimiteRepository>>()
+                .Verify(x =>
+                    x.Incluir(It.IsAny<Limite>())
+                    , Times.Once);
+    }
+
+    [Fact]
+    public async Task IncluirLimite_QuandoLimiteExistente_DisparaExcecao()
+    {
+        var documento = "Documento";
+        var agencia = "Agencia";
+        var conta = "Conta";
+        var valor = 0.00M;
+
+        var limite = _fixture.Create<Limite>();
+        _fixture.Freeze<Mock<ILimiteRepository>>()
+            .Setup(client => client.Buscar(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()))
+            .ReturnsAsync(limite);
+
+        await Assert.ThrowsAsync<Exception>(() => _sut.Create(documento, agencia, conta, valor));
+
+        _fixture.Freeze<Mock<ILimiteRepository>>()
+                .Verify(x =>
+                x.Buscar(
+                    It.IsAny<string>(),
+                    It.IsAny<string>(),
+                    It.IsAny<string>())
+                    , Times.Once);
+        _fixture.Freeze<Mock<ILimiteRepository>>()
+                .Verify(x =>
+                    x.Incluir(It.IsAny<Limite>())
+                    , Times.Never);
+    }
+}
