16373be37b5e3bf2e1ff2dbda7ce9cda994b82fe
Nelson Neto <nelson.ash@outlook.com>
2024-10-28 16:39:17 -0300

    Inclusao de propriedades em Use Case

--- Application/CreateLimiteCommand.cs
+++ Application/CreateLimiteCommand.cs
@@ -1,12 +1,14 @@
 ﻿namespace Application;
 
-public record CreateLimiteCommand();
-public record CreateLimiteResponse();
+public record CreateLimiteCommand(string Documento, string Agencia, string Conta, decimal Valor);
+public record CreateLimiteResponse(Guid Id, string Documento, string Agencia, string Conta, decimal Valor);
 
 public class CreateLimiteHandler
 {
-    public async Task<CreateLimiteResponse> Create(CreateLimiteCommand command, CancellationToken cancellationToken = default)
+    public async Task<CreateLimiteResponse> Handle(CreateLimiteCommand command, CancellationToken cancellationToken = default)
     {
+        // Buscar Se Documento Agencia e Conta existem Se não existirem retorna exception Se valor
+        // for menor ou igual a 0
         throw new NotImplementedException();
     }
 }
---
+++ UnitTests/CreateLimiteHandlerUnitTests.cs
@@ -0,0 +1,28 @@
+﻿using Application;
+using AutoFixture;
+
+namespace UnitTests;
+
+public class CreateLimiteHandlerUnitTests : UnitTestsBase
+{
+    private readonly CreateLimiteHandler _sut;
+    private readonly CreateLimiteCommand _command;
+
+    public CreateLimiteHandlerUnitTests()
+    {
+        _command = _fixture.Create<CreateLimiteCommand>();
+        _sut = _fixture.Create<CreateLimiteHandler>();
+    }
+
+    [Fact]
+    public async Task IncluirLimite_DadosValidos_RetornaDadosInseridos()
+    {
+        var response = await _sut.Handle(_command, CancellationToken.None);
+
+        Assert.NotEqual(Guid.Empty, response.Id);
+        Assert.Equal(_command.Documento, response.Documento);
+        Assert.Equal(_command.Agencia, response.Agencia);
+        Assert.Equal(_command.Conta, response.Conta);
+        Assert.Equal(_command.Valor, response.Valor);
+    }
+}
