ed4102fd9a90e187eb9870f10f6555a7e9a8a31e
Nelson Neto <nelson.ash@outlook.com>
2024-10-28 15:57:50 -0300

    Utilizacao do AutoFixture

--- UnitTests/LimiteRepositoryUnitTests.cs
+++ UnitTests/LimiteRepositoryUnitTests.cs
@@ -1,5 +1,7 @@
 using Amazon.DynamoDBv2;
 using Amazon.DynamoDBv2.Model;
+using AutoFixture;
+using AutoFixture.AutoMoq;
 using Domain.Entities;
 using Domain.Exceptions;
 using Infrastructure.Repositories;
@@ -10,25 +12,31 @@ namespace UnitTests;
 
 public class LimiteRepositoryUnitTests
 {
+    private readonly IFixture _fixture = new Fixture()
+        .Customize(new AutoMoqCustomization
+        {
+            ConfigureMembers = true
+        });
+
     private readonly LimiteRepository _sut;
-    public Mock<IAmazonDynamoDB> dynamoDbMock { get; set; }
 
     public LimiteRepositoryUnitTests()
     {
-        dynamoDbMock = new Mock<IAmazonDynamoDB>();
-        _sut = new LimiteRepository(dynamoDbMock.Object);
-    }
-
-    [Fact]
-    public async Task IncluirLimite_DadosValidos_RetornaDadosInseridos()
-    {
-        dynamoDbMock.Setup(client => client.PutItemAsync(
+        _fixture.Freeze<Mock<IAmazonDynamoDB>>()
+            .Setup(client => client.PutItemAsync(
                It.IsAny<PutItemRequest>(),
                It.IsAny<CancellationToken>()))
                .ReturnsAsync((PutItemRequest r, CancellationToken token) =>
                {
                    return new PutItemResponse { HttpStatusCode = HttpStatusCode.OK };
                });
+
+        _sut = _fixture.Create<LimiteRepository>();
+    }
+
+    [Fact]
+    public async Task IncluirLimite_DadosValidos_RetornaDadosInseridos()
+    {
         var limite = new Limite("Documento", "Agencia", "Conta", 0.00M);
 
         limite = await _sut.Incluir(limite);
@@ -39,12 +47,20 @@ public class LimiteRepositoryUnitTests
         Assert.Equal("Agencia", limite.Agencia);
         Assert.Equal("Conta", limite.Conta);
         Assert.Equal(0.00M, limite.Valor);
+
+        _fixture.Freeze<Mock<IAmazonDynamoDB>>()
+                .Verify(x =>
+                    x.PutItemAsync(
+                        It.IsAny<PutItemRequest>(),
+                        It.IsAny<CancellationToken>())
+                    , Times.Once);
     }
 
     [Fact]
     public async Task IncluirLimite_ProblemaContexto_DisparaExcecaoContexto()
     {
-        dynamoDbMock.Setup(client => client.PutItemAsync(
+        _fixture.Freeze<Mock<IAmazonDynamoDB>>()
+            .Setup(client => client.PutItemAsync(
                It.IsAny<PutItemRequest>(),
                It.IsAny<CancellationToken>()))
                .ReturnsAsync((PutItemRequest r, CancellationToken token) =>
@@ -54,5 +70,11 @@ public class LimiteRepositoryUnitTests
         var limite = new Limite("Documento", "Agencia", "Conta", 0.00M);
 
         await Assert.ThrowsAsync<ContextoException>(() => _sut.Incluir(limite));
+        _fixture.Freeze<Mock<IAmazonDynamoDB>>()
+                .Verify(x =>
+                    x.PutItemAsync(
+                        It.IsAny<PutItemRequest>(),
+                        It.IsAny<CancellationToken>())
+                    , Times.Once);
     }
 }
