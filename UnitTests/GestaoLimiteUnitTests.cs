68dcacf3873410556c1ccb6b24dea39518737003
Nelson Neto <nelson.ash@outlook.com>
2024-10-28 12:19:07 -0300

    baby steps to Limite entity

--- UnitTests/GestaoLimiteUnitTests.cs
+++ UnitTests/GestaoLimiteUnitTests.cs
@@ -1,10 +1,53 @@
 namespace UnitTests;
 
-public class GestaoLimiteUnitTests
+public class UnitTestsBase
+{
+    public Contexto contexto { get; set; }
+
+    public UnitTestsBase()
+    {
+        contexto = new Contexto();
+    }
+}
+
+public class GestaoLimiteUnitTests : UnitTestsBase
 {
     [Fact]
-    public void TesteInicial_RetornaFalha()
+    public void AdicionarLimite_DadosValidos_RetornaDadosInseridos()
     {
-        Assert.Fail();
+        var limite = new Limite(0, "Documento", "Agencia", "Conta", 0.00M);
+        limite = contexto.Incluir(limite);
+        Assert.Equal(1, limite.Id);
+        Assert.Equal("Documento", limite.Documento);
+        Assert.Equal("Agencia", limite.Agencia);
+        Assert.Equal("Conta", limite.Conta);
+        Assert.Equal(0.00M, limite.Valor);
     }
+}
+
+public class Contexto
+{
+    public Limite Incluir(Limite limite)
+    {
+        limite = new Limite(1, limite.Documento, limite.Agencia, limite.Conta, limite.Valor);
+        return limite;
+    }
+}
+
+public class Limite
+{
+    public Limite(int id, string documento, string agencia, string conta, decimal valor)
+    {
+        Id = id;
+        Documento = documento;
+        Agencia = agencia;
+        Conta = conta;
+        Valor = valor;
+    }
+
+    public int Id { get; private set; }
+    public string Documento { get; private set; }
+    public string Agencia { get; private set; }
+    public string Conta { get; private set; }
+    public decimal Valor { get; private set; }
 }
