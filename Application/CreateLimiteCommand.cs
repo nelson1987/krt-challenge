3bed22c636078df45dc146466f20dff7c8ae87ae
Nelson Neto <nelson.ash@outlook.com>
2024-10-28 16:25:37 -0300

    Inclusao de Application

---
+++ Application/CreateLimiteCommand.cs
@@ -0,0 +1,12 @@
+﻿namespace Application;
+
+public record CreateLimiteCommand();
+public record CreateLimiteResponse();
+
+public class CreateLimiteHandler
+{
+    public async Task<CreateLimiteResponse> Create(CreateLimiteCommand command, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+}
