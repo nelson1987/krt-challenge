0dd7eafeaf98f48a5671babce1a7593558dd13ad
Nelson Neto <nelson.ash@outlook.com>
2025-05-02 19:16:39 -0300

    Atualizar o IProducer.cs

--- messagebus/IProducer.cs
+++ messagebus/IProducer.cs
@@ -2,3 +2,12 @@ namespace MessageBus.Producer;
 public interface IProducer {}
 public interface IConsumer {}
 public interface IEvent {}
+
+public record Event : IEvent {}
+public class Producer : IProducer {}
+public class Consumer : IConsumer {}
+
+public class Handler{
+public void Handler(){
+}
+}
