396d2545bc411088546feb5e673d3b59b4274b20
Nelson Neto <nelson.ash@outlook.com>
2025-05-02 19:19:50 -0300

    Atualizar o IProducer.cs

--- messagebus/IProducer.cs
+++ messagebus/IProducer.cs
@@ -4,10 +4,13 @@ public interface IConsumer {}
 public interface IEvent {}
 
 public record Event : IEvent {}
-public class Producer : IProducer {}
+public class Producer : IProducer {
+public void SendAsync();
+}
 public class Consumer : IConsumer {}
 
-public class Handler{
+public class Handler(IProducer producer){
 public void Handler(){
+producer.SendAsync();
 }
 }
